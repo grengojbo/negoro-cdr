241dc5c31a5ae593bc051230933c601d71bd5af7
Oleg Dolya <oleg.dolya@gmail.com>
2012-07-24 21:38:08 +0300

    init repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BilliCdr::Application.config.secret_token = '6968e26c581e7931c9bd6a9b7c493d155a4e8312c1c05eb3e4524e4dd50d655ce5af99fc8d21412f9034a0657a05122edd1253f3c5103608445bb5ccf0e16cca'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+BilliCdr::Application.config.session_store :cookie_store, key: '_billi-cdr_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# BilliCdr::Application.config.session_store :active_record_store
